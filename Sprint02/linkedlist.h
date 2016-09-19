e2dad01ae09628c8c0d4de063dc2416b9a9acf4c
Mark <markefontenot@gmail.com>
2016-09-19 08:09:45 -0500

    Add files via upload

---
+++ Sprint02/linkedlist.h
@@ -0,0 +1,46 @@
+#ifndef LINKED_LIST
+#define LINKED_LIST
+
+template <class T>
+class ListNode {
+public:
+
+
+private:
+    ListNode<T>*next;
+    ListNode<T>*last;
+    T data;
+};
+
+
+template<class T>
+class LinkedList {
+
+public:
+
+    LinkedList();
+    LinkedList(T);
+    LinkedList(const LinkedList<T>&);
+
+    void add(T);
+    void addToFront(T);
+    T get(int);
+    int size();
+    T remove(int);
+    bool isEmpty();
+    
+    ~LinkedList();
+
+    T& operator[] (int);
+    LinkedList<T>& operator=(LinkedList<T>&);
+
+private:
+
+    ListNode<T>*head;
+    int num_elements;
+
+};
+
+//Write your implementation here
+
+#endif
---
+++ Sprint02/tests.cpp
@@ -0,0 +1,83 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include "linkedlist.h"
+#include "queue.h"
+#include "stack.h"
+
+TEST_CASE("LinkedList class", "[LinkedList]"){
+
+    Section("Default constructor"){
+
+    }
+
+    Section("Constructor with head"){
+
+    }
+
+    Section("Copy constructor"){
+
+    }
+
+    Section("add"){
+
+    }
+
+    Section("addToFront"){
+
+    }
+
+    Section("get"){
+
+    }
+
+    Section("size"){
+
+    }
+
+    Section("remove"){
+
+    }
+
+    Section("[] operator"){
+
+    }
+
+    Section("assignment operator"){
+
+    }
+
+
+}
+
+TEST_CASE("Stack class", "[stack]"){
+
+    Section("Push"){
+
+    }
+
+    Section("Pop"){
+
+    }
+
+    Section("Peek"){
+
+    }
+
+}
+
+TEST_CASE("Queue class", "[queue]"){
+
+    Section("Enqueue"){
+
+    }
+
+    Section("Dequeue"){
+
+    }
+
+    Section("Peek"){
+
+    }
+
+}
